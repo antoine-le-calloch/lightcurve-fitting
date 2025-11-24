23262f254c10370adcf06485cda5860f813b7d05
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-11-24 11:50:30 -0500

    Add script to sanitize 'fp_hists' entries in JSON by removing those without 'magpsf' key.

---
+++ format_json.py
@@ -0,0 +1,16 @@
+import json
+
+# Remove entries in 'fp_hists' that do not contain 'magpsf' key
+if __name__ == "__main__":
+    json_file = "candidates/ZTF25acdhetm.json"
+    with open(json_file, "r") as file:
+        candidate = json.loads(file.read())
+
+    sanitize_fp_hists = []
+    for fp in candidate["fp_hists"]:
+        if "magpsf" in fp:
+            sanitize_fp_hists.append(fp)
+
+    candidate["fp_hists"] = sanitize_fp_hists
+    with open(json_file, "w") as file:
+        json.dump(candidate, file, indent=2)
