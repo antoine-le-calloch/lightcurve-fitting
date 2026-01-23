dc32b0ba9a411ef28980803c95932060772f9b85
james-freeburn <jfreeburn@Scron.localdomain>
2026-01-23 16:27:02 +1100

    Implemented separate upper limits analysis, now prints to a csv file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use serde_json::from_reader;
 use crate::models::{PerBandProperties, AllBandsProperties};
 use crate::ztf_enrichment::{get_alert_properties, ZtfAlertForEnrichment};
+use std::io::Write;
 
 mod ztf_enrichment;
 mod models;
@@ -69,16 +70,64 @@ fn print_properties(photstats: &PerBandProperties, all_bands_props: &AllBandsPro
     }
 }
 
+fn save_candidate_properties(candidate_names: &[String], photstats_vec: &[PerBandProperties], filename: &str) {
+    let mut file = File::create(filename).expect("Failed to create CSV file");
+    
+    // Write header
+    writeln!(file, "candidate_name,band,peak_jd,peak_mag,peak_mag_err,rising_rate,rising_r_squared,rising_nb_data,fading_rate,fading_r_squared,fading_nb_data").expect("Failed to write header");
+    
+    // Write data for each band
+    for (photstats, candidate_name) in photstats_vec.iter().zip(candidate_names.iter()) {
+        let bands = [
+            ("g", &photstats.g),
+            ("r", &photstats.r),
+            ("i", &photstats.i),
+            ("z", &photstats.z),
+            ("y", &photstats.y),
+            ("u", &photstats.u),
+        ];
+        
+        for (band_name, band_props) in bands {
+            if let Some(bp) = band_props {
+                let rising_rate = bp.rising.as_ref().map(|r| r.rate.to_string()).unwrap_or_default();
+                let rising_r2 = bp.rising.as_ref().map(|r| r.r_squared.to_string()).unwrap_or_default();
+                let rising_nb = bp.rising.as_ref().map(|r| r.nb_data.to_string()).unwrap_or_default();
+                
+                let fading_rate = bp.fading.as_ref().map(|f| f.rate.to_string()).unwrap_or_default();
+                let fading_r2 = bp.fading.as_ref().map(|f| f.r_squared.to_string()).unwrap_or_default();
+                let fading_nb = bp.fading.as_ref().map(|f| f.nb_data.to_string()).unwrap_or_default();
+                
+                writeln!(file, "{},{},{},{},{},{},{},{},{},{},{}",
+                    candidate_name, band_name, bp.peak_jd, bp.peak_mag, bp.peak_mag_err,
+                    rising_rate, rising_r2, rising_nb,
+                    fading_rate, fading_r2, fading_nb
+                ).expect("Failed to write data");
+            }
+        }
+    }
+}
+
 fn main() {
+    let mut photstats_vec: Vec<PerBandProperties> = Vec::new();
+    let mut photstatsul_vec: Vec<PerBandProperties> = Vec::new();
+    let mut candidate_names: Vec<String> = Vec::new();
     for entry in std::fs::read_dir("candidates/").expect("Failed to read candidates directory") {
         let entry = entry.expect("Failed to read directory entry");
         let path = entry.path();
+
         // process only json files
         if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("json") {
+            candidate_names.push(String::from(path.file_stem().unwrap().to_str().unwrap()));
             let alert = load_ztf_candidate(path.to_str().unwrap()).expect("Failed to load ZTF candidate");
-            let (alert_props, all_bands_props, _, _) = get_alert_properties(&alert);
+            let (alert_props, alert_props_ul, all_bands_props, _, _) = get_alert_properties(&alert);
 
             print_properties(&alert_props.photstats, &all_bands_props, &path);
+            println!("---------USING UPPER LIMITS--------");
+            print_properties(&alert_props_ul.photstats, &all_bands_props, &path);
+            photstats_vec.push(alert_props.photstats);
+            photstatsul_vec.push(alert_props_ul.photstats);
         }
     }
+    save_candidate_properties(&candidate_names, &photstats_vec, "candidate_properties.csv");
+    save_candidate_properties(&candidate_names, &photstatsul_vec, "candidate_properties_ul.csv");
 }
